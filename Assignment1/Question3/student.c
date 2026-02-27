de51ea2aa09eb5f6871aabb9e4c3315e122fff3d
jamesmarti025 <jamespmartin125@gmail.com>
2026-02-26 23:33:48 -0600

    Finish Assignment

--- Assignment1/Question3/student.c
+++ Assignment1/Question3/student.c
@@ -26,8 +26,30 @@
 //Output parameter (returnSize): set *returnSize to the number of digits in the returned array.
 
 int* plusOne(int* digits, int digitsSize, int* returnSize) {
-    // TODO: implement
 
-    
+    int* result = (int*)malloc((digitsSize + 1) * sizeof(int));
+
+    int carry = 1;
+
+    for (int i = digitsSize - 1; i >= 0; i--) {
+
+        int sum = digits[i] + carry;
+
+        result[i + 1] = sum % 10;
+        carry = sum / 10;
+    }
+
+    if (carry == 1) {
+        result[0] = 1;
+        *returnSize = digitsSize + 1;
+        return result;
+    }
+
+    for (int i = 0; i < digitsSize; i++) {
+        result[i] = result[i + 1];
+    }
+
+    *returnSize = digitsSize;
+    return result;
 }
 
